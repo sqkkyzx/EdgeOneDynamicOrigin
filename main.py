43f69f50c0f167a3b3983d47b846e11358ac1879
sqkkyzx <admin@sqkkyzx.com>
2024-06-26 07:40:26 +0800

    修改日志记录

--- main.py
+++ main.py
@@ -17,20 +17,16 @@ def setup_logging():
     _logger = logging.getLogger()
     _logger.setLevel(logging.INFO)
 
-    # 创建文件处理器并设置级别
     file_handler = RotatingFileHandler("eodo.log", maxBytes=200 * 1024, backupCount=1)
     file_handler.setLevel(logging.INFO)
 
-    # 创建控制台处理器并设置级别
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
 
-    # 创建格式化器并将其添加到处理器
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
 
-    # 将处理器添加到记录器
     _logger.addHandler(file_handler)
     _logger.addHandler(console_handler)
 
